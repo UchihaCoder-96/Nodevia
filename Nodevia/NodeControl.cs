222ace9897062c370c9fad82059b68e3f28afa25
UchihaCoder-96 <animeshorts359@gmail.com>
2026-09-02 00:14:56 +0530

    Add First 'NodeControl' and Demo App.

---
+++ Nodevia/NodeControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Nodevia.Controls
+{
+    public class NodeControl : Control
+    {
+        static NodeControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(NodeControl),
+                new FrameworkPropertyMetadata(typeof(NodeControl)));
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(
+                nameof(Title),
+                typeof(string),
+                typeof(NodeControl),
+                new FrameworkPropertyMetadata("Node"));
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+    }
+}
+
