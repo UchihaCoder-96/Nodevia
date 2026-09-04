0c36fbf34b6aab5b97036a73b255951c606eefce
UchihaCoder-96 <animeshorts359@gmail.com>
2026-09-04 12:31:03 +0530

    Add PortControl.

---
+++ Nodevia/Controls/PortControl.cs
@@ -0,0 +1,29 @@
+﻿using Nodevia.Models;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Nodevia.Controls;
+
+public class PortControl : Control
+{
+    static PortControl()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(
+            typeof(PortControl),
+            new FrameworkPropertyMetadata(typeof(PortControl)));
+    }
+
+    public static readonly DependencyProperty PortProperty =
+        DependencyProperty.Register(
+            nameof(Port),
+            typeof(Port),
+            typeof(PortControl),
+            new FrameworkPropertyMetadata(null));
+
+    public Port? Port
+    {
+        get => (Port?)GetValue(PortProperty);
+        set => SetValue(PortProperty, value);
+    }
+}
+
