b5797dd4c71d9d93260a65d53c111e423128724f
UchihaCoder-96 <animeshorts359@gmail.com>
2026-09-12 20:22:43 +0530

    Add toast control for warning and errors.

---
+++ Nodevia/Controls/ToastHost.cs
@@ -0,0 +1,55 @@
+﻿using Nodevia.Diagnostics;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Nodevia.Controls;
+
+public class ToastHost : Control
+{
+    static ToastHost()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(
+            typeof(ToastHost),
+            new FrameworkPropertyMetadata(typeof(ToastHost)));
+    }
+
+    public static readonly DependencyProperty LogProperty =
+        DependencyProperty.Register(nameof(Log), typeof(GraphLog), typeof(ToastHost),
+            new FrameworkPropertyMetadata(null, OnLogChanged));
+
+    public GraphLog? Log
+    {
+        get => (GraphLog?)GetValue(LogProperty);
+        set => SetValue(LogProperty, value);
+    }
+
+    public ObservableCollection<ToastItem> Toasts { get; } = new();
+
+    private static void OnLogChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var host = (ToastHost)d;
+
+        if (e.OldValue is GraphLog oldLog)
+            oldLog.Entries.CollectionChanged -= host.OnEntriesChanged;
+
+        if (e.NewValue is GraphLog newLog)
+            newLog.Entries.CollectionChanged += host.OnEntriesChanged;
+    }
+
+    private void OnEntriesChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e.NewItems is null)
+            return;
+
+        foreach (LogEntry entry in e.NewItems)
+        {
+            if (entry.Severity is LogSeverity.Warning or LogSeverity.Error)
+                Toasts.Add(new ToastItem(entry, Dismiss));
+        }
+    }
+
+    private void Dismiss(ToastItem toast) => Toasts.Remove(toast);
+}
+
---
+++ Nodevia/Diagnostics/ToastItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Input;
+
+namespace Nodevia.Diagnostics;
+
+public class ToastItem
+{
+    public LogEntry Entry { get; }
+    public ICommand DismissCommand { get; }
+
+    public ToastItem(LogEntry entry, Action<ToastItem> onDismiss)
+    {
+        Entry = entry;
+        DismissCommand = new RelayCommand(() => onDismiss(this));
+    }
+}
+
+
+internal class RelayCommand : ICommand
+{
+    private readonly Action _execute;
+
+    public RelayCommand(Action execute) => _execute = execute;
+
+    public event EventHandler? CanExecuteChanged { add { } remove { } }
+    public bool CanExecute(object? parameter) => true;
+    public void Execute(object? parameter) => _execute();
+}
+
